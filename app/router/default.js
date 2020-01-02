0b7338fe3fa727cba89faf5f6e29956945210938
扬 <>
2020-01-02 19:06:40 +0800

    路由设置

--- app/router/default.js
+++ app/router/default.js
@@ -3,6 +3,8 @@
 /**
  * @param {Egg.Application} app - egg application
  */
+
 module.exports = app => {
-  require('./router/default')(app);
+  const { router, controller } = app;
+  router.get('/default/index', controller.default.home.index);
 };
