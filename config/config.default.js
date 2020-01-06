6bc75021127e6da8974c1562bbbfd90d559ed22f
扬 <>
2020-01-06 11:41:26 +0800

    增加egg-mysql

--- config/config.default.js
+++ config/config.default.js
@@ -23,6 +23,27 @@ module.exports = appInfo => {
     // myAppName: 'egg',
   };
 
+  // 设置mysql链接配置
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: 'mysql.com',
+      // port
+      port: '3306',
+      // username
+      user: 'test_user',
+      // password
+      password: 'test_password',
+      // database
+      database: 'test',
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
+
   return {
     ...config,
     ...userConfig,
