9ea14a24ac4781049ff5aa5ec0e4fbb10a82bf7f
yang yang <1075361899@qq.com>
2020-01-06 21:39:41 +0800

    SQL处理时间格式

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -8,13 +8,13 @@ class HomeController extends Controller {
   }
 
   async getArticleList() {
-    let sql = 'SELECT article.id as id, '+
-              'article.title as title, '+
-              'article.introduce as introduce, '+
-              'article.add_time as add_time, '+
-              'article.view_count as view_count, '+
-              'type.type_name as type_name '+
-              'FROM article LEFT JOIN type ON article.type_id = type.Id'
+    let sql = "SELECT article.id as id, "+
+              "article.title as title, "+
+              "article.introduce as introduce, "+
+              "FROM_UNIXTIME(article.add_time, '%Y-%m-%d %H:%i:%s') as add_time, "+
+              "article.view_count as view_count, "+
+              "type.type_name as type_name "+
+              "FROM article LEFT JOIN type ON article.type_id = type.Id"
     const results = await this.app.mysql.query(sql)
     this.ctx.body = {
       data: results,
