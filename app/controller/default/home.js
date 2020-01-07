6b68524acf1f0bfa1039f5d7751675e7e7b457b7
yang yang <1075361899@qq.com>
2020-01-07 21:55:38 +0800

    前台接口调试完成

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -7,6 +7,7 @@ class HomeController extends Controller {
     this.ctx.body = 'hello api';
   }
 
+  // 获取文章列表
   async getArticleList() {
     let sql = "SELECT article.id as id, "+
               "article.title as title, "+
@@ -23,6 +24,7 @@ class HomeController extends Controller {
     }
   }
 
+  // 根据文章ID查询文章详情
   async getArticleById() {
     let id = this.ctx.params.id
     let sql = "SELECT article.id as id, "+
@@ -42,6 +44,36 @@ class HomeController extends Controller {
       msg: 'success'
     }
   }
+
+  // 获取头部类别
+  async getTypeInfo() {
+    const result = await this.app.mysql.select('type')
+    this.ctx.body = {
+      data: result,
+      code: 1,
+      msg: 'success'
+    }
+  }
+
+  // 根据类别ID查询获得文章列表
+  async getListById() {
+    let id = this.ctx.params.id
+    let sql = "SELECT article.id as id, "+
+              "article.title as title, "+
+              "article.introduce as introduce, "+
+              "FROM_UNIXTIME(article.add_time, '%Y-%m-%d %H:%i:%s') as add_time, "+
+              "article.view_count as view_count, "+
+              "type.type_name as type_name "+
+              "FROM article LEFT JOIN type ON article.type_id = type.Id "+
+              "WHERE type_id=" + id
+    const result = await this.app.mysql.query(sql)
+    this.ctx.body = {
+      data: result,
+      code: 1,
+      msg: 'success'
+    }
+  }
+
 }
 
 module.exports = HomeController;
